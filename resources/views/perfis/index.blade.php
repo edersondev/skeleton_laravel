a3034dd6da6f6cc9a613afa6ccfa8de56952f068
Ederson Ferreira <ederson.dev@gmail.com>
2018-09-20 11:48:31 -0300

    atualização do componente datatables

--- resources/views/perfis/index.blade.php
+++ resources/views/perfis/index.blade.php
@@ -1,15 +1,7 @@
 @extends('layouts.app')
 
-@push('css')
-	<link rel="stylesheet" href="{{ URL::asset('components/datatables/css/dataTables.bootstrap4.min.css') }}">
-@endpush
-
-@push('js')
-	<script src="{{ URL::asset('components/datatables/js/jquery.dataTables.min.js') }}"></script>
-	<script src="{{ URL::asset('components/datatables/js/dataTables.bootstrap4.min.js') }}"></script>
-	<script src="{{ URL::asset('js/datatables-plugins/cache/pipelining.js') }}"></script>
-	<script src="{{ URL::asset('components/jquery-dateFormat/jquery-dateformat.min.js') }}"></script>
-@endpush
+@component('components/datatable/assets')
+@endcomponent
 
 @section('title')
 	<i class="fa fa-key"></i> Perfis
