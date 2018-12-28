de4f3d381dcccf953e27eb37f3b22435eb25c789
Ederson Ferreira <ederson.dev@gmail.com>
2018-12-28 10:30:04 -0200

    atualização com componente datepicker e composer.json

--- resources/views/components/form/datepicker.blade.php
+++ resources/views/components/form/datepicker.blade.php
@@ -10,21 +10,25 @@
 			$attributes['aria-describedby'] = "{$name}HelpBlock";
 		}
 	@endphp
-	<div class="input-group date">
+
+	<div class="input-group">
 		{{ Form::text($name, $value, ( isset($attributes) ? array_merge( ['class' => $classInput], $attributes ) : ['class' => $classInput] ) ) }}
 		<div class="input-group-append">
-			<span class="input-group-text"><i class="fas fa-calendar-alt"></i></span>
+			<span class="input-group-text" id="basic-addon2">
+				<i class="far fa-calendar-alt"></i>
+			</span>
 		</div>
-		@if ($errors->has($name))
-			<div class="invalid-feedback">
-				<strong>{{ $errors->first($name) }}</strong>
-			</div>
-		@endif
-
-		@if($helpText and !$errors->has($name))
-			<small id="{{$name}}HelpBlock" class="form-text text-muted">
-				{!! $helpText !!}
-			</small>
-		@endif
 	</div>
+	@if ($errors->has($name))
+		<div class="invalid-feedback">
+			<strong>{{ $errors->first($name) }}</strong>
+		</div>
+	@endif
+
+	@if($helpText and !$errors->has($name))
+		<small id="{{$name}}HelpBlock" class="form-text text-muted">
+			{!! $helpText !!}
+		</small>
+	@endif
+
 </div>
